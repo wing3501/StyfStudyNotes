4eb7e9f36a690f85e0e1d00fc64832e7f92f4823
styf <shentyf@weipaitang.com>
2021-04-23 09:17:01 +0800

    代码提交

--- StyfStudyNotes/StyfStudyNotes/leetcode/动态规划.swift
+++ StyfStudyNotes/StyfStudyNotes/leetcode/动态规划.swift
@@ -42,9 +42,31 @@ import Foundation
 //        print(minDistance("sea", "eat"))//2
         
 //        712. 两个字符串的最小ASCII删除和
-        print(minimumDeleteSum("sea", "eat"))//231
-        print(minimumDeleteSum("delete", "leet"))//403
+//        print(minimumDeleteSum("sea", "eat"))//231
+//        print(minimumDeleteSum("delete", "leet"))//403
     }
+//    516. 最长回文子序列
+//    给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+//    示例 1:
+//    输入:
+//    "bbbab"
+//    输出:
+//    4
+//    一个可能的最长回文子序列为 "bbbb"。
+//    示例 2:
+//    输入:
+//    "cbbd"
+//    输出:
+//    2
+//    一个可能的最长回文子序列为 "bb"。
+//    提示：
+//    1 <= s.length <= 1000
+//    s 只包含小写英文字母
+//    链接：https://leetcode-cn.com/problems/longest-palindromic-subsequence
+    class func longestPalindromeSubseq(_ s: String) -> Int {
+
+    }
+    
 //    712. 两个字符串的最小ASCII删除和
 //    给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
 //    示例 1:
