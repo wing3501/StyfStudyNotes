014f4a9a22911344896db9d9adc655e512a86220
styf <shentyf@weipaitang.com>
2021-06-10 16:52:42 +0800

    代码提交

--- StyfStudyNotes/my_flutter/lib/main.dart
+++ StyfStudyNotes/my_flutter/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_flutter/utils/color_utils.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
