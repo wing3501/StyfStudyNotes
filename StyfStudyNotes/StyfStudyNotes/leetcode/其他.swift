5ec2a26c618c095447a74cce49db07c5f5a7b31c
styf <shentyf@weipaitang.com>
2021-10-26 09:30:46 +0800

    代码提交

--- StyfStudyNotes/StyfStudyNotes/leetcode/其他.swift
+++ StyfStudyNotes/StyfStudyNotes/leetcode/其他.swift
@@ -55,10 +55,73 @@ import Foundation
 //        print(allPathsSourceTarget([[1],[]]))//[[0,1]]
 //        print(allPathsSourceTarget([[1,2,3],[2],[3],[]]))//[[0,1,2,3],[0,2,3],[0,3]]
 //        print(allPathsSourceTarget([[1,3],[2],[3],[]]))//[[0,1,2,3],[0,3]]
-        print(allPathsSourceTarget([[3,1],[4,6,7,2,5],[4,6,3],[6,4],[7,6,5],[6],[7],[]]))
-        
-//        [[0,3,6,7],[0,3,4,7],[0,3,4,6,7],[0,3,4,5,6,7],[0,1,4,7],[0,1,4,6,7],[0,1,4,5,6,7],[0,1,6,7],[0,1,7],[0,1,2,4,7],[0,1,2,4,6,7],[0,1,2,4,5,6,7],[0,1,2,6,7],[0,1,2,3,4,5,6,7],[0,1,5,6,7]]
-//        [[0,3,6,7],[0,3,4,7],[0,3,4,6,7],[0,3,4,5,6,7],[0,1,4,7],[0,1,4,6,7],[0,1,4,5,6,7],[0,1,6,7],[0,1,7],[0,1,2,4,7],[0,1,2,4,6,7],[0,1,2,4,5,6,7],[0,1,2,6,7],[0,1,2,3,6,7],[0,1,2,3,4,7],[0,1,2,3,4,6,7],[0,1,2,3,4,5,6,7],[0,1,5,6,7]]
+//        print(allPathsSourceTarget([[3,1],[4,6,7,2,5],[4,6,3],[6,4],[7,6,5],[6],[7],[]]))
+//        print(allPathsSourceTarget([[5,4,1,2,3],[2,5],[4,5,3],[6,5,4],[5,6],[6],[]]))
+   
+    }
+//    210. 课程表 II
+//    现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，其中 prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
+//    例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+//    返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+//    示例 1：
+//    输入：numCourses = 2, prerequisites = [[1,0]]
+//    输出：[0,1]
+//    解释：总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+//    示例 2
+//    输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+//    输出：[0,2,1,3]
+//    解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+//    因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+//    示例 3：
+//    输入：numCourses = 1, prerequisites = []
+//    输出：[0]
+//    提示：
+//    1 <= numCourses <= 2000
+//    0 <= prerequisites.length <= numCourses * (numCourses - 1)
+//    prerequisites[i].length == 2
+//    0 <= ai, bi < numCourses
+//    ai != bi
+//    所有[ai, bi] 匹配 互不相同
+//    拓展：
+//
+//    这个问题相当于查找一个循环是否存在于有向图中。如果存在循环，则不存在拓扑排序，因此不可能选取所有课程进行学习。
+//    通过 DFS 进行拓扑排序 - 一个关于Coursera的精彩视频教程（21分钟），介绍拓扑排序的基本概念。
+//    拓扑排序也可以通过 BFS 完成。
+//    链接：https://leetcode-cn.com/problems/course-schedule-ii
+    class func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+        return []
+    }
+//    207. 课程表
+//    你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+//    在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+//    例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+//    请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+//    示例 1：
+//    输入：numCourses = 2, prerequisites = [[1,0]]
+//    输出：true
+//    解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+//    示例 2：
+//    输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+//    输出：false
+//    解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+//    提示：
+//    1 <= numCourses <= 105
+//    0 <= prerequisites.length <= 5000
+//    prerequisites[i].length == 2
+//    0 <= ai, bi < numCourses
+//    prerequisites[i] 中的所有课程对 互不相同
+//    https://leetcode-cn.com/problems/course-schedule/
+    class func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        var linjie: [Set<Int>] = Array(repeating: [], count: numCourses)
+        var visited: Set<Int> = []
+        for array in prerequisites {
+            let from = array[1]
+            let to = array[0]
+            visited.insert(from)
+            visited.insert(to)
+            
+        }
+        return visited.count == numCourses
     }
     
 //    797. 所有可能的路径
@@ -94,29 +157,39 @@ import Foundation
             return graph
         }
         var result: [[Int]] = []
-        var dic: [Int:[Int]] = [:]
+        var dic: [Int:[[Int]]] = [:]
         let firstArr = graph[0]
         var stack: [Int] = []
         allPathsSourceTargetHelper(&stack, graph, firstArr, &result, &dic)
         return result
     }
     
-    class func allPathsSourceTargetHelper(_ stack: inout [Int],_ graph: [[Int]],_ array: [Int],_ result: inout [[Int]],_ dic: inout [Int:[Int]]) {
+    class func allPathsSourceTargetHelper(_ stack: inout [Int],_ graph: [[Int]],_ array: [Int],_ result: inout [[Int]],_ dic: inout [Int:[[Int]]]) {
         for item in array {
             if item == graph.count - 1 {
                 stack.append(item)
                 var res = [0]
                 res.append(contentsOf: stack)
                 result.append(res)
-                dic[stack[0]] = stack
+                if dic[stack[0]] == nil {
+                    dic[stack[0]] = []
+                }
+                dic[stack[0]]!.append(stack)
                 stack.removeLast()
             }else {
                 if let cacheArray = dic[item] {
-                    var res = [0]
-                    res.append(contentsOf: stack)
-                    res.append(contentsOf: cacheArray)
-                    result.append(res)
-                    dic[stack[0]] = res
+                    for arr in cacheArray {
+                        var temp = stack
+                        temp.append(contentsOf: arr)
+                        
+                        var res = [0]
+                        res.append(contentsOf: temp)
+                        result.append(res)
+                        if dic[stack[0]] == nil {
+                            dic[stack[0]] = []
+                        }
+                        dic[stack[0]]!.append(temp)
+                    }
                 }else {
                     let indexArray = graph[item]
                     if !indexArray.isEmpty {
