7357a912b70b33a525f587d3eea53f2dc7c772c7
申屠云飞 <shentyf@weipaitang.com>
2022-05-10 16:56:48 +0800

    提交代码

--- StyfStudyNotes/StyfStudyNotes/SwiftDemo/SwiftUI/《SwiftUI by Example》/View/CheckoutView.swift
+++ StyfStudyNotes/StyfStudyNotes/SwiftDemo/SwiftUI/《SwiftUI by Example》/View/CheckoutView.swift
@@ -11,20 +11,63 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
     
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
+    
+    @State private var showingPaymentAlert = false
     
     var body: some View {
-        VStack {
+        // 表单的使用、双向绑定、表单内组件、Section对组件的改变
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
                 }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())//加点顺滑动画
+
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section(header:
+                Text("TOTAL: \(totalPrice)")
+            ) {
+                Button("Confirm order") { //如果你点击它，整行闪烁灰色。这是SwiftUI表单系统改变其内部组件的设计和行为的另一个例子
+                    showingPaymentAlert.toggle()
+                }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        // alert的使用
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) – thank you!"), dismissButton: .default(Text("OK")))
+        }
+    }
+    //格式化数字的使用
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency //格式化数字为金额
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
     }
 }
 
