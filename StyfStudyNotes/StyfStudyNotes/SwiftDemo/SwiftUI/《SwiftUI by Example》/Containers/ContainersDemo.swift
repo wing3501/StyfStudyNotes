01bc39ee018d25a4011f27de581ac3be5ae7588e
申屠云飞 <shentyf@weipaitang.com>
2022-05-16 19:22:27 +0800

    提交代码

---
+++ StyfStudyNotes/StyfStudyNotes/SwiftDemo/SwiftUI/《SwiftUI by Example》/Containers/ContainersDemo.swift
@@ -0,0 +1,20 @@
+//
+//  ContainersDemo.swift
+//  StyfStudyNotes
+//
+//  Created by styf on 2022/5/16.
+//
+
+import SwiftUI
+
+struct ContainersDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContainersDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        ContainersDemo()
+    }
+}
---
+++ StyfStudyNotes/StyfStudyNotes/SwiftDemo/SwiftUI/《SwiftUI by Example》/Forms/FormsDemo.swift
@@ -0,0 +1,148 @@
+//
+//  FormsDemo.swift
+//  StyfStudyNotes
+//
+//  Created by styf on 2022/5/16.
+//
+
+import SwiftUI
+
+struct FormsDemo: View {
+    var body: some View {
+        //显示或隐藏某一行
+        HidingFormRows()
+        //禁用表单元素
+//        DisablingElements()
+        //Pickers在表单中的特殊表现
+//        PickersInForms()
+        //表单分区
+//        FormSections()
+        //表单的基本使用
+//        BasicForm()
+    }
+}
+//-----------------------------
+
+struct HidingFormRows: View {
+    @State private var showingAdvancedOptions = false
+    @State private var enableLogging = false
+    var body: some View {
+        TestWrap("显示或隐藏某一行") {
+            Form {
+                Section {
+                    Toggle("Show advanced options", isOn: $showingAdvancedOptions.animation())
+
+                    if showingAdvancedOptions {
+                        Toggle("Enable logging", isOn: $enableLogging)
+                    }
+                }
+            }
+        }
+    }
+}
+//-----------------------------
+struct DisablingElements: View {
+    //SwiftUI允许我们使用disabled（）修饰符禁用其窗体的任何部分，甚至整个窗体
+    @State private var agreedToTerms = false
+    var body: some View {
+        TestWrap("禁用表单元素") {
+            Form {
+                Section {
+                    Toggle("Agree to terms and conditions", isOn: $agreedToTerms)
+                }
+
+                Section {
+                    Button("Continue") {
+                        print("Thank you!")
+                    }
+                    .disabled(agreedToTerms == false)
+                }
+            }
+        }
+    }
+}
+
+
+
+//-----------------------------
+struct PickersInForms: View {
+    @State private var selectedStrength = "Mild"
+    let strengths = ["Mild", "Medium", "Mature"]
+    var body: some View {
+        TestWrap("Pickers在表单中的特殊表现") {
+            NavigationView {
+                Form {
+                    Section {
+                        Picker("Strength", selection: $selectedStrength) {
+                            ForEach(strengths, id: \.self) {
+                                Text($0)
+                            }
+                        }
+//                        .pickerStyle(.wheel) //强制修改为滚轮
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+//-----------------------------
+struct FormSections: View {
+    @State private var enableLogging = false
+    @State private var selectedColor = "Red"
+    @State private var colors = ["Red", "Green", "Blue"]
+    var body: some View {
+        TestWrap("表单分区") {
+            Form {
+                Section(footer: Text("Note: Enabling logging may slow down the app")) {
+                    Picker("Select a color", selection: $selectedColor) {
+                        ForEach(colors, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+
+                    Toggle("Enable Logging", isOn: $enableLogging)
+                }
+
+                Section {
+                    Button("Save changes") {
+                        // activate theme!
+                    }
+                }
+            }
+        }
+    }
+}
+//-----------------------------
+struct BasicForm: View {
+    @State private var enableLogging = false
+    @State private var selectedColor = "Red"
+    @State private var colors = ["Red", "Green", "Blue"]
+    var body: some View {
+        TestWrap("表单的基本使用") {
+            Form {
+                Picker("Select a color", selection: $selectedColor) {
+                    ForEach(colors, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+
+                Toggle("Enable Logging", isOn: $enableLogging)
+
+                Button("Save changes") {
+                    // activate theme!
+                }
+            }
+        }
+    }
+}
+//-----------------------------
+struct FormsDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        FormsDemo()
+    }
+}
