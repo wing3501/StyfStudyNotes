36478e0d366f634758bce11804c1d51574b0a5f6
申屠云飞 <shentyf@weipaitang.com>
2022-09-13 16:09:22 +0800

    Table排序的使用

--- 《macOS by Tutorials》/OnThisDay/OnThisDay/OnThisDay/Views/TableView.swift
+++ 《macOS by Tutorials》/OnThisDay/OnThisDay/OnThisDay/Views/TableView.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 struct TableView: View {
     var tableData: [Event]
+    // ✅ Table排序的使用 1
+    @State private var sortOrder = [KeyPathComparator(\Event.year)]
+    
+    var sortedTableData: [Event] {//Table排序的使用 4 使用排序描述
+        return tableData.sorted(using: sortOrder)
+    }
     
     var body: some View {
-        // ✅ Table
-        Table(tableData) {
-            TableColumn("Year") {
+        // ✅ Table的使用
+        Table(sortedTableData,sortOrder: $sortOrder) { //Table排序的使用 2 绑定
+            TableColumn("Year",value: \.year) { //Table排序的使用 3 设置keypath
                 Text($0.year)
             }
             .width(min: 50, ideal: 60, max: 100) // 防止拖动分隔线把一侧变得过大或者过小
-            TableColumn("Title") {
-                Text($0.text)
-            }
+//            TableColumn("Title") {
+//                Text($0.text)
+//            }
+            TableColumn("Title", value: \.text)// 简写
+            
         }
     }
 }
