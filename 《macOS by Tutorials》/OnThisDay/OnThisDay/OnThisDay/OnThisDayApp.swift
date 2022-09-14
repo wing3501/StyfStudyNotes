90dd782001333980d7b9cc01a4534cfefa5974c2
申屠云飞 <shentyf@weipaitang.com>
2022-09-14 17:06:17 +0800

    应用图标

--- 《macOS by Tutorials》/OnThisDay/OnThisDay/OnThisDay/OnThisDayApp.swift
+++ 《macOS by Tutorials》/OnThisDay/OnThisDay/OnThisDay/OnThisDayApp.swift
@@ -14,6 +14,8 @@ import SwiftUI
 // Mac 默认情况下会禁用网络连接
 
 // ✅修改target名称，可以便捷地使用带空格的应用名   也可以修改Build Setting中的Product Name
+// ✅macOS app的不会自动对应用图标做圆角处理
+// ✅使用Bakery生成图标
 
 @main
 struct OnThisDayApp: App {
