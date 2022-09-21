b829efdaee8aeb65eb714b4e3ead0acd25f30ce9
申屠云飞 <shentyf@weipaitang.com>
2022-09-21 17:26:22 +0800

    切换源码与预览

--- 《macOS by Tutorials》/MarkDowner/MarkDowner/ContentView.swift
+++ 《macOS by Tutorials》/MarkDowner/MarkDowner/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+enum PreviewState {
+    case web
+    case code
+    case off
+}
+
 struct ContentView: View {
     @Binding var document: MarkDownerDocument
+    
+    @State private var previewState = PreviewState.web
 
     var body: some View {
         HSplitView {
             TextEditor(text: $document.text)
                 .frame(minWidth: 200)
-            WebView(html: document.html)// ✅ WKWebview加载本地HTML，依赖需要打开沙盒权限
-                .frame(minWidth: 200)
+            if previewState == .web {
+                WebView(html: document.html)// ✅ WKWebview加载本地HTML，依赖需要打开沙盒权限
+                    .frame(minWidth: 200)
+            }else if previewState == .code {
+                ScrollView {
+                    Text(document.html)
+                        .frame(minWidth: 200)
+                        .frame(maxWidth: .infinity,
+                               maxHeight: .infinity,
+                               alignment: .topLeading)
+                        .padding()
+                        .textSelection(.enabled)
+                }
+            }
         }
         .frame(minWidth: 400,
                idealWidth: 600,
@@ -23,6 +43,21 @@ struct ContentView: View {
                minHeight: 300,
                idealHeight: 400,
                maxHeight: .infinity)
+        .toolbar {
+            // 工具栏项目-切换源码和预览
+            ToolbarItem {
+                Picker("", selection: $previewState) {
+                    Image(systemName: "network")
+                        .tag(PreviewState.web)
+                    Image(systemName: "chevron.left.forwardslash.chevron.right")
+                        .tag(PreviewState.code)
+                    Image(systemName: "nosign")
+                        .tag(PreviewState.off)
+                }
+                .pickerStyle(.segmented)
+                .help("Hide preview,show Html or web view") //可达性提示条
+            }
+        }
     }
 }
 
