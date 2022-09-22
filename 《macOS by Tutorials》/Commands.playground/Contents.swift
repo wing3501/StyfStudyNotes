2e40de76f63f9f81c305572f4dcc46e9f7c77704
申屠云飞 <shentyf@weipaitang.com>
2022-09-22 16:46:17 +0800

    Process的基本使用

--- 《macOS by Tutorials》/Commands.playground/Contents.swift
+++ 《macOS by Tutorials》/Commands.playground/Contents.swift
@@ -1,9 +1,46 @@
 import Cocoa
 
+func getAvailableData(from fileHandle: FileHandle) -> String {
+    let newData = fileHandle.availableData
+    if let string = String(data: newData, encoding: .utf8) {
+        return string
+    }
+    return ""
+}
+
 let process = Process()
 
-process.executableURL = URL(fileURLWithPath: "/usr/bin/whoami")
+process.executableURL = URL(fileURLWithPath: "/sbin/ping")
+
+// ✅ 传入参数
+process.arguments = ["-c","5","baidu.com"]
+
+// ✅ 使用自己的输出管道
+let outPipe = Pipe()
+let outFile = outPipe.fileHandleForReading
+process.standardOutput = outPipe
 
 
+// ✅ 等待执行结束，一次性读取所有数据
+//do {
+//    try? process.run()
+//    process.waitUntilExit()
+//    // ✅ 获取输出
+//    if let data = try outFile.readToEnd(),
+//       let returnValue = String(data: data, encoding: .utf8) {
+//        print("Result: \(returnValue)")
+//    }
+//}catch {
+//    print(error)
+//}
 
-try? process.run()
+// ✅ 一点点读取数据
+
+try process.run()
+
+while process.isRunning {
+    let newString = getAvailableData(from: outFile)
+    print(newString.trimmingCharacters(in: .whitespacesAndNewlines))
+}
+let newString = getAvailableData(from: outFile)
+print(newString.trimmingCharacters(in: .whitespacesAndNewlines))
