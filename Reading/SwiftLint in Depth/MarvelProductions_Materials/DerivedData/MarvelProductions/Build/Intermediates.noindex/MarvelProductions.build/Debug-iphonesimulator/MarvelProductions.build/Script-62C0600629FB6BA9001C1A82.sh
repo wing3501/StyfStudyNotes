42031f277109af07e48b2f740db727a5c0f53196
申屠云飞 <174481103@qq.com>
2023-04-28 14:44:22 +0800

    SwiftLint in Depth

--- Reading/SwiftLint in Depth/MarvelProductions_Materials/DerivedData/MarvelProductions/Build/Intermediates.noindex/MarvelProductions.build/Debug-iphonesimulator/MarvelProductions.build/Script-62C0600629FB6BA9001C1A82.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export PATH="$PATH:/opt/homebrew/bin"
-if which swiftlint > /dev/null; then
-  swiftlint
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
-
