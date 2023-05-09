d776fd7e716b89255ef0ec869471cbfbce105819
申屠云飞 <174481103@qq.com>
2023-05-09 15:35:13 +0800

    自定义下拉刷新

--- 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/ScrollViewGeometryReader.swift
+++ 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/ScrollViewGeometryReader.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2022 Kodeco Inc.
-///
+/// Copyright (c) 2023 Kodeco Inc.
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,58 +32,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State var events: [Event] = []
+struct ScrollViewGeometryReader: View {
+  @Binding var pullToRefresh: PullToRefresh
   
-  @State var pullToRefresh = PullToRefresh(progress: 0, state: .idle)
-
-  var body: some View {
-    testBody
-    
-//    List(events) {
-//      EventView(event: $0)
-//        .listRowSeparator(.hidden)
-//    }
-//    .refreshable {
-//      await update()
-//    }
-//    .listStyle(.plain)
-//    .navigationBarTitleDisplayMode(.inline)
-//    .onAppear {
-//      Task {
-//        await update()
-//      }
-//    }
-  }
+  let update: () async -> Void
   
-  var testBody: some View {
-    ScrollView {
-      ScrollViewGeometryReader(pullToRefresh: $pullToRefresh) {
-        await update()
-        print("Update!")
+  @State private var startOffset: CGFloat = 0
+  
+    var body: some View {
+      GeometryReader<Color> { proxy in
+        Task {
+          // 因为内部会修改状态，所以使用异步
+          calculateOffset(from:proxy)
+        }
+        return Color.clear
       }
-      ZStack(alignment: .top) {
-        if pullToRefresh.state == .ongoing {
-          ProgressView()
-        }
-        LazyVStack {
-          ForEach(events) {
-            EventView(event: $0)
-          }
-        }
-        .offset(y: pullToRefresh.state == .ongoing ? Constants.maxOffset : 0)
+      .task {
+        await update()
       }
     }
-  }
-
-  @MainActor
-  func update() async {
-    events = await fetchMoreEvents(toAppend: events)
-  }
-}
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
+  
+  private func calculateOffset(from proxy: GeometryProxy) {
+    // 当前下拉位置
+    let currentOffset = proxy.frame(in: .global).minY
+    
+    switch pullToRefresh.state {
+    case .idle://开始下拉
+      startOffset = currentOffset
+      pullToRefresh.state = .pulling
+      
+    case .pulling where pullToRefresh.progress < 1:
+      pullToRefresh.progress = min(1, (currentOffset - startOffset) / Constants.maxOffset)
+      
+    case .pulling:
+      pullToRefresh.state = .ongoing//触发刷新
+      pullToRefresh.progress = 0
+      Task {
+        await update()
+        pullToRefresh.state = .idle
+      }
+      
+    default: return
+    }
   }
 }
