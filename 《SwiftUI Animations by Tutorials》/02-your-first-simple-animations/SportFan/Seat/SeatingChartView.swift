d079d8bac71fe10bdbdfee8b6932489459d5c68e
申屠云飞 <174481103@qq.com>
2023-05-18 16:48:41 +0800

    横排座位

--- 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/Seat/SeatingChartView.swift
+++ 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/Seat/SeatingChartView.swift
@@ -35,13 +35,19 @@ import SwiftUI
 struct SeatingChartView: View {
   
   @State private var field = CGRect.zero
+  @State private var tribunes: [Int: [Tribune]] = [:]
   
     var body: some View {
       ZStack {
         Field().path(in: field).fill(.green)
         Field().path(in: field).stroke(.white, lineWidth: 2)
-        Stadium(field: $field)
+        Stadium(field: $field, tribunes: $tribunes)
           .stroke(.white, lineWidth: 2)
+        
+        ForEach(tribunes.flatMap(\.value),id: \.self) { tribune in
+          tribune.path
+            .stroke(.white, style: StrokeStyle(lineWidth: 1, lineJoin: .round))
+        }
       }
     }
 }
@@ -49,6 +55,7 @@ struct SeatingChartView: View {
 struct Stadium: Shape {
   
   @Binding var field: CGRect
+  @Binding var tribunes: [Int: [Tribune]]
   
   func path(in rect: CGRect) -> Path {
     Path { path in
@@ -59,6 +66,8 @@ struct Stadium: Shape {
       
       var smallestSctorFrame = CGRect.zero
       
+      let tribuneSize = CGSize(width: sectorDiff / 3, height: sectorDiff / 4.5)
+      
       (0..<Constants.stadiumSectorsCount).forEach { i in
         let sectionWidth = width - sectorDiff * Double(i)
         let sectionHeight = width / widthToHeightRatio - sectorDiff * Double(i)
@@ -68,7 +77,16 @@ struct Stadium: Shape {
         let sectorRect = CGRect(x: offsetX, y: offsetY, width: sectionWidth, height: sectionHeight)
         smallestSctorFrame = sectorRect
         
-        path.addRoundedRect(in: sectorRect, cornerSize: CGSize(width: sectorRect.width / 4.0, height: sectorRect.width / 4.0), style: .continuous)
+        //path.addRoundedRect(in: sectorRect, cornerSize: CGSize(width: sectorRect.width / 4.0, height: sectorRect.width / 4.0), style: .continuous)
+        
+        let tribuneWidthOffset = (tribuneSize.width / CGFloat(Constants.stadiumSectorsCount * 2)) * Double(i)
+        path.addPath(Sector(tribunes: $tribunes,
+                            index: i,
+                            tribuneSize: CGSize(width: tribuneSize.width - tribuneWidthOffset, height: tribuneSize.height),
+                            offset: (sectorDiff / 2 - tribuneSize.height) / 2.0
+                           )
+          .path(in: sectorRect)
+        )
       }
       
       computeField(in: smallestSctorFrame)
@@ -94,8 +112,83 @@ struct Field: Shape {
       path.move(to: CGPoint(x: rect.midX, y: rect.minY))
       path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
       path.move(to: CGPoint(x: rect.midX, y: rect.midY))
-      path.addEllipse(in: CGRect(x: rect.midX - rect.width / 8.0, y: rect.midY - rect.width / 8.0, width: rect.width / 4.0, height: rect.width / 4.0))
+      path.addEllipse(in: CGRect(x: rect.midX - rect.width / 8.0,
+                                 y: rect.midY - rect.width / 8.0,
+                                 width: rect.width / 4.0,
+                                 height: rect.width / 4.0))
     }
+  }
+}
+
+struct Tribune: Hashable, Equatable {
+  var path: Path
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(path.description)
+  }
+}
+
+struct RectTribune: Shape {
+  func path(in rect: CGRect) -> Path {
+    Path { path in
+      path.addRect(rect)
+      path.closeSubpath()
+    }
+  }
+}
+
+struct Sector: Shape {
+  @Binding var tribunes: [Int: [Tribune]]
+  var index: Int
+  var tribuneSize: CGSize
+  var offset: CGFloat
+  
+  func path(in rect: CGRect) -> Path {
+    Path { path in
+      let corner = rect.width / 4.0
+      
+      path.addRoundedRect(in: rect,
+                          cornerSize: CGSize(width: corner, height: corner),
+                          style: .continuous)
+      
+      guard !tribunes.keys.contains(where: { $0 == index}) else {
+        return
+      }
+      Task {
+        tribunes[index] = computeRectTribunesPaths(at: rect, corner: corner)
+        
+      }
+    }
+  }
+  
+  private func computeRectTribunesPaths(at rect: CGRect, corner: CGFloat) -> [Tribune] {
+    let segmentWidth = rect.width - corner * 2.0
+    let segmentHeight = rect.height - corner * 2.0
+    
+    let tribunesHorizontalCount = segmentWidth / tribuneSize.width
+    let tribunesVerticalCount = segmentHeight / tribuneSize.width
+    
+    let spacingH = (segmentWidth - tribuneSize.width * tribunesHorizontalCount) / tribunesHorizontalCount
+    let spacingV = (segmentHeight - tribuneSize.width * tribunesVerticalCount) / tribunesVerticalCount
+    
+    var tribunes = [Tribune]()
+    (0..<Int(tribunesHorizontalCount)).forEach { i in
+      let x = rect.minX + (tribuneSize.width + spacingH) * CGFloat(i) + corner + spacingH / 2
+      tribunes.append(makeRectTribuneAt(x: x, y: rect.minY + offset))
+      tribunes.append(makeRectTribuneAt(x: x, y: rect.maxY - offset - tribuneSize.height))
+    }
+    
+    (0..<Int(tribunesVerticalCount)).forEach { i in
+      let y = rect.minY + (tribuneSize.width + spacingV) * CGFloat(i) + corner + spacingV / 2
+      tribunes.append(makeRectTribuneAt(x: rect.minX + offset, y: y, rotated: true))
+      tribunes.append(makeRectTribuneAt(x: rect.maxX - offset - tribuneSize.height, y: y, rotated: true))
+    }
+    
+    return tribunes
+  }
+  
+  private func makeRectTribuneAt(x: CGFloat, y: CGFloat, rotated: Bool = false) -> Tribune {
+    Tribune(path: RectTribune().path(in: CGRect(x: x, y: y, width: rotated ? tribuneSize.height : tribuneSize.width, height: rotated ? tribuneSize.width : tribuneSize.height)))
   }
 }
 
