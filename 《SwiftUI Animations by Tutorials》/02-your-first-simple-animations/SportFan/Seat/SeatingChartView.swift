ee5e0246192995f33791485d33338d59836471db
申屠云飞 <174481103@qq.com>
2023-05-19 14:19:36 +0800

    圆弧的座位

--- 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/Seat/SeatingChartView.swift
+++ 《SwiftUI Animations by Tutorials》/02-your-first-simple-animations/SportFan/Seat/SeatingChartView.swift
@@ -155,8 +155,8 @@ struct Sector: Shape {
         return
       }
       Task {
-        tribunes[index] = computeRectTribunesPaths(at: rect, corner: corner)
-        
+//        tribunes[index] = computeRectTribunesPaths(at: rect, corner: corner)
+        tribunes[index] = computeTribunes(at: rect, with: corner)
       }
     }
   }
@@ -187,8 +187,71 @@ struct Sector: Shape {
     return tribunes
   }
   
+  func computeArcTribunesPaths(at rect: CGRect, corner: CGFloat) -> [Tribune] {
+    let radius = corner - offset
+    let innerRadius = corner - offset - tribuneSize.height
+    
+    let arcLength = (.pi / 2) * radius
+    let arcTribunesCount = Int(arcLength / (tribuneSize.width * 1.2))
+    
+    let arcSpacing = (arcLength - tribuneSize.width * CGFloat(arcTribunesCount)) / CGFloat(arcTribunesCount + 1)
+    let angle = tribuneSize.width / radius
+    let spacingAngle = arcSpacing / radius
+    
+    let arcs: [CGFloat: CGPoint] = [
+      .pi: CGPoint(x: rect.minX + corner, y: rect.minY + corner), // 1
+      3 * .pi / 2: CGPoint(x: rect.maxX - corner, y: rect.minY + corner), // 2
+      2 * .pi: CGPoint(x: rect.maxX - corner, y: rect.maxY - corner), // 3
+      5 * .pi / 2: CGPoint(x: rect.minX + corner, y: rect.maxY - corner) // 4
+    ]
+    
+    return arcs.reduce(into: [Tribune]()) { tribunes, arc in
+      var previousAngle = arc.key
+      let center = arc.value
+      
+      let arcTribunes = (0..<arcTribunesCount).map { _ in
+        let startingPoint = CGPoint(x: center.x + radius * cos(previousAngle + spacingAngle),
+                                    y: center.y + radius * sin(previousAngle + spacingAngle))
+        let startingInnerPoint = CGPoint(x: center.x + innerRadius * cos(previousAngle + spacingAngle + angle),
+                                         y: center.y + innerRadius * sin(previousAngle + spacingAngle + angle))
+        
+        let tribune = Tribune(path: ArcTribune(center: center, radius: radius, innerRadius: innerRadius, startingPoint: startingPoint, startingInnerPoint: startingInnerPoint, startAngle: previousAngle + spacingAngle, endAngle: previousAngle + spacingAngle + angle)
+          .path(in: CGRect.zero)
+        )
+        previousAngle += spacingAngle + angle
+        return tribune
+      }
+      
+      tribunes.append(contentsOf: arcTribunes)
+    }
+  }
+  
   private func makeRectTribuneAt(x: CGFloat, y: CGFloat, rotated: Bool = false) -> Tribune {
     Tribune(path: RectTribune().path(in: CGRect(x: x, y: y, width: rotated ? tribuneSize.height : tribuneSize.width, height: rotated ? tribuneSize.width : tribuneSize.height)))
+  }
+  
+  private func computeTribunes(at rect: CGRect, with corner: CGFloat) -> [Tribune] {
+    computeRectTribunesPaths(at: rect, corner: corner) + computeArcTribunesPaths(at: rect, corner: corner)
+  }
+}
+
+struct ArcTribune: Shape {
+  var center: CGPoint
+  var radius: CGFloat
+  var innerRadius: CGFloat
+  var startingPoint: CGPoint
+  var startingInnerPoint: CGPoint
+  var startAngle: CGFloat
+  var endAngle: CGFloat
+  
+  func path(in rect: CGRect) -> Path {
+    Path { path in
+      path.move(to: startingPoint)
+      path.addArc(center: center, radius: radius, startAngle: .radians(startAngle), endAngle: .radians(endAngle), clockwise: false)
+      path.addLine(to: startingInnerPoint)
+      path.addArc(center: center, radius: innerRadius, startAngle: .radians(endAngle), endAngle: .radians(startAngle), clockwise: true)
+      path.closeSubpath()
+    }
   }
 }
 
