c7a67b6684429083c72e4e18ce595dc9661e4670
申屠云飞 <174481103@qq.com>
2024-03-14 11:23:02 +0800

    导入leaf

--- 《Full Stack iOS Development with Swift and Vapor Full stack iOS development made easy》/Hello/Package.swift
+++ 《Full Stack iOS Development with Swift and Vapor Full stack iOS development made easy》/Hello/Package.swift
@@ -9,12 +9,14 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf")
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
